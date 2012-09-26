c09b80c8e0356e99a48d3847e6eb9a66977aa17a
Tim Diggins <tim@red56.co.uk>
2012-09-26 13:50:20 +0100

    making it possible to install via pip

---
+++ README.txt
@@ -0,0 +1,3 @@
+A bunch of django utils
+
+particularly useful for testing (to bring some helpful rails/webrat-gleaned idioms into django)
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(
+    name = 'tims_django_utils',
+    packages = ['tims_django_utils'],
+    version = '0.1',
+    description = 'A bunch of django utils, mostly for testing',
+    author='Tim Diggins',
+)
